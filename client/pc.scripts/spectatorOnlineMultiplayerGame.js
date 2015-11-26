9c366db771c8363bf49b612b7bde0fcd5c37b2bc
Vasyl Pedak <truvazia@gmail.com>
2015-11-26 23:02:30 +0200

    Prepare server to work in the web

--- client/pc.scripts/spectatorOnlineMultiplayerGame.js
+++ client/pc.scripts/spectatorOnlineMultiplayerGame.js
@@ -1,22 +1,22 @@
 pc.script.create('spectatorOnlineMultiplayerGame', function (app) {
-    // Creates a new SpectatorOnlineMultiplayerGame instance
-    var SpectatorOnlineMultiplayerGame = function (entity) {
-        this.entity = entity;
-        this.game = null;
-    };
+  // Creates a new SpectatorOnlineMultiplayerGame instance
+  var SpectatorOnlineMultiplayerGame = function (entity) {
+    this.entity = entity;
+    this.game = null;
+  };
 
-    SpectatorOnlineMultiplayerGame.prototype = {
-        // Called once after all resources are loaded and before the first update
-        initialize: function () {
-            this.game = new Game.Multiplayer.Online.Spectator(app);
-            this.game.initialize();
-        },
+  SpectatorOnlineMultiplayerGame.prototype = {
+    // Called once after all resources are loaded and before the first update
+    initialize: function () {
+      this.game = new Game.Multiplayer.Online.Spectator(app);
+      this.game.initialize();
+    },
 
-        // Called every frame, dt is time in seconds since last update
-        update: function (dt) {
-            this.game.update(dt);
-        }
-    };
+    // Called every frame, dt is time in seconds since last update
+    update: function (dt) {
+      this.game.update(dt);
+    }
+  };
 
-    return SpectatorOnlineMultiplayerGame;
+  return SpectatorOnlineMultiplayerGame;
 });
--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+var _ = require('underscore');
 var app = require('http').createServer(handler);
 var io = require('socket.io')(app);
 var fs = require('fs');
@@ -18,18 +19,49 @@ function handler (req, res) {
       });
 }
 
-var totalFrames = 0, client = null;
+var activityCheckDelay = 2000,
+    slave,
+    master,
+    lastTick;
+
+io.origins('localhost:* playcanvas.com:80 playcanv.as:80');
+
 io.on('connection', function (socket) {
   socket.emit('greeting', { hello: 'world' });
-  if (!client) {
-    client = socket;
-  } else {
-    socket.on('tick', function (data) {
+  if (!master) {
+    master = socket;
+    console.log('Master connected');
+    master.on('tick', function (data) {
       //var currentFramesCount = data.length;
       //totalFrames += currentFramesCount;
       //console.log('current frames received: ' + currentFramesCount);
       //console.log('last dt: ' + data[0].dt + ' last time: ' + data[0].time + ' frames total: ' + totalFrames);
-      client.emit('new_frame', {frames: data});
+      lastTick = _.now();
+
+      if(slave)
+        slave.emit('new_frame', {frames: data});
     });
+  } else {
+    console.log('Slave connected');
+    slave = socket;
   }
 });
+
+setInterval(function() {
+  if (master && slave && _.now() - lastTick >= activityCheckDelay) {
+
+    console.log('Activity timed out');
+
+    if (master) {
+      master.disconnect();
+      master = null;
+      console.log('Master disconnected');
+    }
+
+    if (slave) {
+      slave.disconnect();
+      slave = null;
+      console.log('Slave disconnected');
+    }
+  }
+}, activityCheckDelay);
