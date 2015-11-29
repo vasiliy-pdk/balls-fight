08c23fd2809668a9a896807674fe64d84f946310
Vasyl Pedak <truvazia@gmail.com>
2015-11-30 01:59:13 +0200

    Refactor storable module to the CommonJS

--- build/scripts/balls-fight.js
+++ build/scripts/balls-fight.js
@@ -467,96 +467,91 @@ GameStatePlayer.prototype = {
 module.exports = GameStatePlayer;
 
 },{"../../../app":1,"./storable":11}],11:[function(require,module,exports){
-module.exports = (function(app, _) {
-  var storablesRegistry = {};
+var storablesRegistry = {};
 
-  var FrameStorable = function(entity, config) {
-    this.entity = entity;
-    this.state = null;
+var FrameStorable = function(entity, config) {
+  this.entity = entity;
+  this.state = null;
 
-    if(config) {
-      if(config.getState) {
-        this.getState = config.getState;
-      }
-
-      if(config.restore) {
-        this.restore = config.restore;
-      }
-    }
-  };
-
-  FrameStorable.prototype = {
-    store: function() {
-      this.state = this.getState();
-    },
-
-    restore: function(state) {
-      this.entity.setPosition(this.restoreVector(state.position));
-      this.entity.setEulerAngles(this.restoreVector(state.rotation));
-    },
-
-    getState: function() {
-      var state = {};
-      state.name = this.entity.getName();
-      state.position = this.storeVector(this.entity.getPosition());
-      state.rotation = this.storeVector(this.entity.getEulerAngles());
-      return state;
-    },
-
-    storeVector: function (vec) {
-      return _.object(['x','y','z'], _.values(vec.data));
-    },
-
-    restoreVector: function(state) {
-      return new pc.Vec3(state.x, state.y, state.z);
-    }
-  };
-
-  var RigidBodyFrameStorable = function(entity, config) {
-    FrameStorable.call(this, entity, config);
-  };
-
-  // @TODO: Implement correct inheritance here
-  _.extend(RigidBodyFrameStorable.prototype, FrameStorable.prototype, {
-    getState: function() {
-      var state = FrameStorable.prototype.getState.apply(this);
-      state.linearVelocity = this.storeVector(this.entity.rigidbody.linearVelocity);
-      state.angularVelocity = this.storeVector(this.entity.rigidbody.angularVelocity);
-      return state;
-    },
-
-    restore: function(state) {
-      this.entity.rigidbody.teleport(this.restoreVector(state.position), this.restoreVector(state.rotation));
-      this.entity.rigidbody.linearVelocity = this.restoreVector(state.linearVelocity);
-      this.entity.rigidbody.angularVelocity = this.restoreVector(state.angularVelocity);
-    }
-  });
-
-  FrameStorable.factory = function (entity, config) {
-    var storable = storablesRegistry[entity.getName()];
-
-    if(!storable) {
-      if(entity.rigidbody && !entity.rigidbody.isStaticOrKinematic())
-        storable = new RigidBodyFrameStorable(entity, config);
-      else
-        storable = new FrameStorable(entity, config);
-
-      storablesRegistry[entity.getName()] = storable;
+  if(config) {
+    if(config.getState) {
+      this.getState = config.getState;
     }
 
-    return storable;
-  };
+    if(config.restore) {
+      this.restore = config.restore;
+    }
+  }
+};
 
-  FrameStorable.getAll = function() {
-    return storablesRegistry;
-  };
+FrameStorable.prototype = {
+  store: function() {
+    this.state = this.getState();
+  },
+
+  restore: function(state) {
+    this.entity.setPosition(this.restoreVector(state.position));
+    this.entity.setEulerAngles(this.restoreVector(state.rotation));
+  },
+
+  getState: function() {
+    var state = {};
+    state.name = this.entity.getName();
+    state.position = this.storeVector(this.entity.getPosition());
+    state.rotation = this.storeVector(this.entity.getEulerAngles());
+    return state;
+  },
+
+  storeVector: function (vec) {
+    return _.object(['x','y','z'], _.values(vec.data));
+  },
+
+  restoreVector: function(state) {
+    return new pc.Vec3(state.x, state.y, state.z);
+  }
+};
+
+var RigidBodyFrameStorable = function(entity, config) {
+  FrameStorable.call(this, entity, config);
+};
+
+// @TODO: Implement correct inheritance here
+_.extend(RigidBodyFrameStorable.prototype, FrameStorable.prototype, {
+  getState: function() {
+    var state = FrameStorable.prototype.getState.apply(this);
+    state.linearVelocity = this.storeVector(this.entity.rigidbody.linearVelocity);
+    state.angularVelocity = this.storeVector(this.entity.rigidbody.angularVelocity);
+    return state;
+  },
+
+  restore: function(state) {
+    this.entity.rigidbody.teleport(this.restoreVector(state.position), this.restoreVector(state.rotation));
+    this.entity.rigidbody.linearVelocity = this.restoreVector(state.linearVelocity);
+    this.entity.rigidbody.angularVelocity = this.restoreVector(state.angularVelocity);
+  }
+});
+
+FrameStorable.factory = function (entity, config) {
+  var storable = storablesRegistry[entity.getName()];
+
+  if(!storable) {
+    if(entity.rigidbody && !entity.rigidbody.isStaticOrKinematic())
+      storable = new RigidBodyFrameStorable(entity, config);
+    else
+      storable = new FrameStorable(entity, config);
+
+    storablesRegistry[entity.getName()] = storable;
+  }
+
+  return storable;
+};
+
+FrameStorable.getAll = function() {
+  return storablesRegistry;
+};
   
-  return {
-    FrameStorable: FrameStorable,
-    RigidBodyFrameStorable: RigidBodyFrameStorable
-  };
-
-})(pc.Application.getApplication(), _);
+exports.FrameStorable = FrameStorable;
+exports.RigidBodyFrameStorable = RigidBodyFrameStorable;
 
 },{}]},{},[2])(2)
 });
